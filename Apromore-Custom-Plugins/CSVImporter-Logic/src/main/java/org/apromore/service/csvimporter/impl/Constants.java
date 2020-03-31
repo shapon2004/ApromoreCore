2bce4a86ddb74d084682edf7ff6376bca4f76dad
Ibrahim <i.mahdy93@gmail.com>
2020-03-31 20:14:15 +0300

    use date parse lib - pend testing

--- Apromore-Custom-Plugins/CSVImporter-Logic/src/main/java/org/apromore/service/csvimporter/impl/Constants.java
+++ Apromore-Custom-Plugins/CSVImporter-Logic/src/main/java/org/apromore/service/csvimporter/impl/Constants.java
@@ -22,17 +22,17 @@ package org.apromore.service.csvimporter.impl;
 
 public interface Constants {
 
-    final String caseid    = "caseid";
-    final String activity  = "activity";
-    final String timestamp = "timestamp";
-    final String tsStart   = "startTimestamp";
-    final String tsValue   = "otherTimestamp";
-    final String resource  = "resource";
+    String caseid    = "caseid";
+    String activity  = "activity";
+    String timestamp = "timestamp";
+    String tsStart   = "startTimestamp";
+    String tsValue   = "otherTimestamp";
+    String resource  = "resource";
 
-    final String[] caseIdValues    = {"case", "case id", "case-id", "service id", "event id", "caseid", "serviceid"};
-    final String[] activityValues  = {"activity", "activity id", "activity-id", "operation", "event"};
-    final String[] resourceValues  = {"resource", "agent", "employee", "group"};
-    final String[] timestampValues = {"timestamp", "end date", "complete timestamp", "time:timestamp",
+    String[] caseIdValues    = {"case", "case id", "case-id", "service id", "event id", "caseid", "serviceid"};
+    String[] activityValues  = {"activity", "activity id", "activity-id", "operation", "event"};
+    String[] resourceValues  = {"resource", "agent", "employee", "group"};
+    String[] timestampValues = {"timestamp", "end date", "complete timestamp", "time:timestamp",
             "completion time", "end timestamp"};
-    final String[] StartTsValues   = {"start date", "start timestamp", "start time"};
+    String[] StartTsValues   = {"start date", "start timestamp", "start time"};
 }
