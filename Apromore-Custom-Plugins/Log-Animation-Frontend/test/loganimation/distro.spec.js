3d7837843e7efbdde22d6c9c36880e29a54df88b
Bruce Nguyen <hoangnguyen7x@yahoo.com>
2020-11-22 16:41:16 +1000

    Update.

--- Apromore-Custom-Plugins/Log-Animation-Frontend/test/loganimation/distro.spec.js
+++ Apromore-Custom-Plugins/Log-Animation-Frontend/test/loganimation/distro.spec.js
@@ -1,13 +1,18 @@
-xdescribe('loganimation', function() {
+import LogAnimation from "../../src/loganimation";
 
-    let editorContainer = document.createElement('div');
-    editorContainer.id = 'editorcanvas';
+describe('test library distribution', function () {
+    beforeEach(function() {
+        jasmine.getFixtures().fixturesPath = 'base/test/loganimation/fixtures';
+        loadFixtures('test.html');
+    });
 
-    it('should expose globals', function() {
-        let LogAnimation = logAnimation.LogAnimation;
-        // then
-        //expect(LogAnimation).to.exist;
-        expect(new LogAnimation()).to.exist;
+    it('should expose global entry class', function() {
+        const URL = "http://b3mn.org/stencilset/bpmn2.0#";
+        let bpmn = require('./fixtures/simple.bpmn');
+        let setupData = require('./fixtures/setupData.json');
+        let logAnim = new LogAnimation(bpmn, URL, URL, setupData, '111');
+        expect(logAnim).not.toEqual(null);
+        expect(logAnim).not.toEqual({});
     });
 
 });
