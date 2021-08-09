dae7807871c380e3e9cd99ad7ab2b325518856cb
justinapromore <justin.joseph@apromore.com>
2021-08-09 08:43:26 +0400

    BPMN editor upgrade to 8.7.1 draft

--- Apromore-Core-Components/Apromore-BPMNEditor/src/main/groovy/mergeJs.groovy
+++ Apromore-Core-Components/Apromore-BPMNEditor/src/main/groovy/mergeJs.groovy
@@ -9,6 +9,7 @@ def destJs = new File('.').absolutePath+"/src/main/resources/static/bpmneditor/e
 
 // Name of the output file; will be placed in $bootstrapJS above
 def outputFile = destJs + 'apromore-editor.js'
+def outputFile1 = destJs + 'apromore-editor1.js'
 
 // order matters; popover must come after twipsy
 def scripts = [
@@ -30,6 +31,24 @@ def scripts = [
 'scripts/plugins/view.js'
 ]
 
+def scripts_new = [
+        'scripts/site.properties.js',
+        'scripts/clazz.js',
+        'scripts/config.js',
+//'scripts/editor.js',
+        'scripts/editorapp.js',
+        'scripts/logger.js',
+        'scripts/server.js',
+        'scripts/utils.js',
+        'scripts/plugins/apromoreSave.js',
+        'scripts/plugins/export.js',
+        'scripts/plugins/pdf.js',
+        'scripts/plugins/share.js',
+        'scripts/plugins/simulationPanel.js',
+        'scripts/plugins/toolbar.js',
+        'scripts/plugins/undo.js',
+        'scripts/plugins/view.js'
+]
 
 def scriptText = new StringBuilder()
 scripts.each { scriptText << new File(sourceJs + it).text }
@@ -39,8 +58,15 @@ def compressor = new JavaScriptCompressor( new StringReader(scriptText.toString(
 def compressedText = new StringWriter()
 
 //// Params: Writer, --line-break, --nomunge, --verbose, --preserve-semi, --disable-optimizations
-compressor.compress(compressedText, -1, false, false, false, false)
+compressor.compress(compressedText, -1, false, false, true, true)
 //
 new File(outputFile).write( compressedText.toString() )
+
+scriptText = new StringBuilder()
+scripts_new.each { scriptText << new File(sourceJs + it).text }
+compressor = new JavaScriptCompressor( new StringReader(scriptText.toString()), null)
+compressedText = new StringWriter()
+compressor.compress(compressedText, -1, false, false, true, true)
+new File(outputFile1).write( compressedText.toString() )
 
 println "Bootstrap JS files combined and minified to: ${System.properties['line.separator']}$outputFile"
